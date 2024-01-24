6a1b383915a8baa08d4d85331ac2a981c48161dc
mj010504 <mj010504@naver.com>
2024-01-25 01:17:46 +0900

    stream setting

--- app/src/main/java/com/example/dailymemo/Setting/SettingFragment.kt
+++ app/src/main/java/com/example/dailymemo/Setting/SettingFragment.kt
@@ -33,17 +33,7 @@ class SettingFragment : Fragment() {
     lateinit var binding : FragmentSettingBinding
 
 
-    private val pickImage = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        if (result.resultCode == RESULT_OK) {
-            result.data?.data?.let { uri ->
 
-                val savedImagePath = saveImageToInternalStorage(uri)
-
-                // 저장된 이미지를 불러와서 이미지뷰에 설정
-                loadImageFromInternalStorage(savedImagePath)
-            }
-        }
-    }
 
 
     override fun onCreateView(
@@ -57,12 +47,13 @@ class SettingFragment : Fragment() {
                 findNavController().navigate(R.id.premiumSettingFragment)
             }
 
-            userProfileIv.setOnClickListener {
-                openGallery()
-            }
 
             accountWithdrawalTv.setOnClickListener {
                 findNavController().navigate(R.id.accountWithdrawalFragment)
+            }
+
+            settingIcIv.setOnClickListener {
+                findNavController().navigate(R.id.streamSettingFragment)
             }
         }
 
@@ -80,33 +71,7 @@ class SettingFragment : Fragment() {
         }
     }
 
-    private fun openGallery() {
-        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        pickImage.launch(galleryIntent)
-    }
 
-    // 이미지 저장을 위한 함수
-    private fun saveImageToInternalStorage(uri: Uri): String {
-        val inputStream = requireActivity().contentResolver.openInputStream(uri)
-        val fileName = "user_profile_image.jpg"
-        val file = File(requireContext().filesDir, fileName)
-
-        val outputStream = FileOutputStream(file)
-
-        inputStream?.use { input ->
-            outputStream.use { output ->
-                input.copyTo(output)
-            }
-        }
-
-        // 저장된 파일 경로를 SharedPreferences에 저장
-        val preferences = requireActivity().getPreferences(Context.MODE_PRIVATE)
-        val editor = preferences.edit()
-        editor.putString("user_profile_image_path", file.absolutePath)
-        editor.apply()
-
-        return file.absolutePath
-    }
 
     // 저장된 이미지를 불러와서 이미지뷰에 설정하는 함수
     private fun loadImageFromInternalStorage(filePath: String) {
